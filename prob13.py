242cce18b5d9a1723b0cdf014dd5fe77867d8c89
lukeb2014 <lukebaird84@gmail.com>
2016-03-16 14:55:47 -0700

    Solved problem 13
    Problem was that with declination minute values, I was not checking to
    see if the the degree value was negative. If it was negative, the
    declination minute needed to be changed to negative

--- prob13.py
+++ prob13.py
@@ -1,3 +1,28 @@
+"""numLines = int(input())
+import math
+for i in range(numLines):
+    line = input().split()
+    if ("+" in line[5]):
+        line[5] = line[5][1:]
+    name = line[0]
+    #line = str(line).split()
+    print(line[1])
+    print(line[3])
+    RAh = float(line[1])
+    RAm = float(line[2])
+    decd = float(line[3])
+    decm = float(line[4])
+    LY = float(line[7])
+    RA = 15.0*RAh + 0.25*RAm
+    dec = decd + (decm / 60.0)
+    r = LY
+    theta = (90.0-dec) * math.pi / 180.0
+    phi = RA * math.pi/180.0
+    x = r*math.sin(theta)*math.cos(phi)
+    y = r*math.sin(theta)*math.sin(phi)
+    z = r*math.cos(theta)
+    print(name + " x=" + str(x) + ", y=" + str(y) + ", z=" + str(z))"""
+
 numLines = int(input())
 import math
 for i in range(numLines):
@@ -5,29 +30,24 @@ for i in range(numLines):
     if ("+" in line[5]):
         line[5] = line[5][1:]
     name = line[0]
-    #print("name = " + name)
     #line = str(line).split()
+    print(line[1])
+    print(line[3])
     RAh = float(line[1])
-    #print("RAh = " + str(RAh))
     RAm = float(line[2])
-    #print("RAm = " + str(RAm))
     decd = float(line[3])
-    #print("decd = " + str(decd))
     decm = float(line[4])
-    #print("decm = " + str(decm))
     LY = float(line[7])
-    #print("LY = " + str(LY))
     RA = 15.0*RAh + 0.25*RAm
-    dec = decd + (decm / 60.0)
+    dec = 0
+    if (decd > 0.0):
+        dec = decd + (decm / 60.0)
+    else:
+        dec = decd - (decm / 60.0)
     r = LY
-    theta = (90.0-dec) * 3.14 / 180.0
+    theta = (90.0-dec) * math.pi / 180.0
     phi = RA * math.pi/180.0
-    #print(str(math.sin(theta)))
-    #print(str(math.cos(phi)))
     x = r*math.sin(theta)*math.cos(phi)
     y = r*math.sin(theta)*math.sin(phi)
     z = r*math.cos(theta)
-    x = round(x, 2)
-    y = round(y, 2)
-    z = round(z, 2)
     print(name + " x=" + str(x) + ", y=" + str(y) + ", z=" + str(z))
